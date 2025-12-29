44e6e8fa5140464ef50b4a4ef4d65478caa26707
pmt1754 <pranav.myneni@springernature.com>
2025-12-29 13:34:33 +0530

    Add TestClass with proper logging

---
+++ src/main/java/com/app/test/TestClass.java
@@ -0,0 +1,19 @@
+package com.app.test;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class TestClass {
+    private static final Logger logger = LoggerFactory.getLogger(TestClass.class);
+    
+    public void goodMethod() {
+        // Proper logging instead of System.out.println
+        logger.info("Debug output");
+        
+        try {
+            // some code
+        } catch (Exception e) {
+            logger.error("Error occurred", e);
+        }
+    }
+}
